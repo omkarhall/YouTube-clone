05b852801b2c9ddd057a165ab334442cc4cc8e3e
omkarhall <omkar.hall12@gmail.com>
2025-03-14 19:31:44 -0600

    recommit changes

---
+++ yt-web-client/app/page.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from "./page.module.css";
+import { getVideos } from "./firebase/functions";
+
+export default async function Home() {
+  const videos = await getVideos();
+
+  return (
+    <main>
+      {
+        videos.map((video) => (
+          <Link href={`/watch?v=${video.filename}`}>
+            <Image src={'/thumbnail.png'} alt='video' width={120} height={80}
+              className={styles.thumbnail}/>
+          </Link>
+        ))
+      }
+    </main>
+  )
+}
